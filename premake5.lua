9ee3402824e11b1cfe02f0695719f123cf4a3656
Mercury <56260362+londonmax12@users.noreply.github.com>
2023-02-15 01:24:58 +1100

    Added files

---
+++ premake5.lua
@@ -0,0 +1,83 @@
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+workspace "Boron"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Distribution"
+    }
+
+    project "Boron"
+        location "projects/Boron"
+        kind "StaticLib"
+        language "C++"
+        cppdialect "C++20"
+        staticruntime "off"
+
+        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("bin/int/" .. outputdir .. "/%{prj.name}")
+
+        includedirs
+        {
+            "projects/boron/src",
+        }
+
+        files
+        {
+            "projects/%{prj.name}/src/**.h",
+            "projects/%{prj.name}/src/**.cpp",
+        }
+        filter "configurations:Debug"
+            buildoptions "/MDd"
+            symbols "on"
+
+        filter "configurations:Release"
+            buildoptions "/MD"
+            optimize "on"
+
+        filter "configurations:Distribution"
+            buildoptions "/MD"
+            optimize "on"
+
+    project "Example"
+        location "projects/Example"
+        kind "ConsoleApp"
+        language "C++"
+        cppdialect "C++20"
+        staticruntime "off"
+
+        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("bin/int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "projects/%{prj.name}/src/**.h",
+            "projects/%{prj.name}/src/**.cpp",
+        }
+
+        includedirs
+        {
+            "projects/Boron/src"
+        }        
+
+        links
+        {
+            "Boron"
+        }
+
+        filter "configurations:Debug"
+            buildoptions "/MDd"
+            symbols "on"
+
+        filter "configurations:Release"
+            buildoptions "/MD"
+            optimize "on"
+
+        filter "configurations:Distribution"
+            buildoptions "/MD"
+            optimize "on"
+            
+        
